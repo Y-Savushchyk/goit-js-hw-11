cda72af071434e5cea828b9aee6b39679f69f40d
Y-Savushchyk <savushchky@gmail.com>
2023-03-29 21:35:02 +0300

    add ligthbox js and  change bgc in simple-lightbox.min.css

--- src/js/ligthbox.js
+++ src/js/ligthbox.js
@@ -1,11 +1,11 @@
-
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 let lightbox = new SimpleLightbox('.photo-card a', {
-  captions: true,
-  captionsData: 'alt',
-  captionDelay: 250,
+    //для вывода описания картинки внизу самой картинки при открытии ее в большом формате (в дз нет)
+//   captions: true,
+//   captionsData: 'alt',
+//   captionDelay: 250,
 });
 
 export { lightbox };
